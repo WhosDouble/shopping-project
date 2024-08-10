fa3cd3468a822f76bb787b6f99a96f29d5af92e6
SirRango <sturdivantdevon78@gmail.com>
2024-08-10 16:33:04 -0500

    init repo

---
+++ sp1/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Section1 from "./components/section1";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Section1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ sp1/src/components/section1.jsx
@@ -0,0 +1,8 @@
+function Section1() {
+  return (
+    <section>
+      <h1>The Best collection</h1>
+    </section>
+  );
+}
+export default Section1;
