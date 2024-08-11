0140e65754046b085509fd0d2c880fe06ca15f99
SirRango <sturdivantdevon78@gmail.com>
2024-08-10 23:55:46 -0500

    still broken marquee

--- sp1/src/shared/Marquee.jsx
+++ sp1/src/shared/Marquee.jsx
@@ -1,81 +1,84 @@
 function Marquee() {
   return (
     <>
-      <div className="flex items-center w-full h-[60px] gap-10 bg-[#1b1b1b] overflow-x-hidden mt-10">
-        <div className="flex gap-14 animate-loop-scroll">
-          <div className="flex gap-5">
-            <div className="border p-1 rounded-2xl text-[14px] text-[#eee]">
-              NORTH FACE
+      <div className="flex items-center w-full h-[60px] gap-10 bg-[#1b1b1b] overflow-x-hidden overflow-y-hidden group mt-10">
+        <div className="flex gap-14 animate-loop-scroll group-hover:paused">
+          <div className="flex gap-14 ">
+            <div className="flex gap-20 w-full  ">
+              <div className="border w-full p-1 rounded-2xl text-[13px] text-[#eee]">
+                NORTH FACE
+              </div>
+              <h1 className="text-[#eee] text-[10px]">NEW FALL x COLLECTION</h1>
             </div>
-            <h1 className="text-[#eee]">NEW FALL x COLLECTION</h1>
+            <div className="flex gap-20 w-full p-20">
+              <div className="border p-1 w-full rounded-2xl text-[13px] text-[#eee]">
+                NORTH FACE
+              </div>
+              <h1 className="text-[#eee]">NEW FALL x COLLECTION</h1>
+            </div>
+            <div className="flex gap-20 w-full p-20">
+              <div className="border p-1 w-full rounded-2xl text-[13px] text-[#eee]">
+                NORTH FACE
+              </div>
+              <h1 className="text-[#eee]">NEW FALL x COLLECTION</h1>
+            </div>
+            <div className="flex gap-20 w-full p-20">
+              <div className="border p-1 w-full rounded-2xl text-[13px] text-[#eee]">
+                NORTH FACE
+              </div>
+              <h1 className="text-[#eee]">NEW FALL x COLLECTION</h1>
+            </div>
+            <div className="flex gap-20 w-full p-20">
+              <div className="border p-1 w-full rounded-2xl text-[13px] text-[#eee]">
+                NORTH FACE
+              </div>
+              <h1 className="text-[#eee]">NEW FALL x COLLECTION</h1>
+            </div>
+            <div className="flex gap-20 w-full p-20">
+              <div className="border p-1 w-full rounded-2xl text-[13px] text-[#eee]">
+                NORTH FACE
+              </div>
+              <h1 className="text-[#eee]">NEW FALL x COLLECTION</h1>
+            </div>
           </div>
-          <div className="flex gap-5">
-            <div className="border p-1 rounded-2xl text-[14px] text-[#eee]">
-              NORTH FACE
+
+          <div className="flex gap-14">
+            <div className="flex gap-20 w-full p-20 ">
+              <div className="border p-1 w-full rounded-2xl text-[13px] text-[#eee]">
+                NORTH FACE
+              </div>
+              <h1 className="text-[#eee]">NEW FALL x COLLECTION</h1>
             </div>
-            <h1 className="text-[#eee]">NEW FALL x COLLECTION</h1>
-          </div>
-          <div className="flex gap-5">
-            <div className="border p-1 rounded-2xl text-[14px] text-[#eee]">
-              NORTH FACE
+            <div className="flex gap-20 w-full p-20">
+              <div className="border p-1 w-full rounded-2xl text-[13px] text-[#eee]">
+                NORTH FACE
+              </div>
+              <h1 className="text-[#eee]">NEW FALL x COLLECTION</h1>
             </div>
-            <h1 className="text-[#eee]">NEW FALL x COLLECTION</h1>
-          </div>
-          <div className="flex gap-5">
-            <div className="border p-1 rounded-2xl text-[14px] text-[#eee]">
-              NORTH FACE
+            <div className="flex gap-20 w-full p-20">
+              <div className="border p-1 w-full rounded-2xl text-[13px] text-[#eee]">
+                NORTH FACE
+              </div>
+              <h1 className="text-[#eee]">NEW FALL x COLLECTION</h1>
             </div>
-            <h1 className="text-[#eee]">NEW FALL x COLLECTION</h1>
-          </div>
-          <div className="flex gap-5">
-            <div className="border p-1 rounded-2xl text-[14px] text-[#eee]">
-              NORTH FACE
+            <div className="flex gap-20 w-full p-20">
+              <div className="border p-1 w-full rounded-2xl text-[13px] text-[#eee]">
+                NORTH FACE
+              </div>
+              <h1 className="text-[#eee]">NEW FALL x COLLECTION</h1>
             </div>
-            <h1 className="text-[#eee]">NEW FALL x COLLECTION</h1>
-          </div>
-          <div className="flex gap-5">
-            <div className="border p-1 rounded-2xl text-[14px] text-[#eee]">
-              NORTH FACE
+            <div className="flex gap-20 w-full p-20">
+              <div className="border p-1 w-full rounded-2xl text-[13px] text-[#eee]">
+                NORTH FACE
+              </div>
+              <h1 className="text-[#eee]">NEW FALL x COLLECTION</h1>
             </div>
-            <h1 className="text-[#eee]">NEW FALL x COLLECTION</h1>
-          </div>
-        </div>
-        <div className="flex gap-14 animate-loop-scroll">
-          <div className="flex gap-5 ">
-            <div className="border p-1 rounded-2xl text-[14px] text-[#eee]">
-              NORTH FACE
+            <div className="flex gap-20 w-full p-20">
+              <div className="border p-1 w-full rounded-2xl text-[13px] text-[#eee]">
+                NORTH FACE
+              </div>
+              <h1 className="text-[#eee]">NEW FALL x COLLECTION</h1>
             </div>
-            <h1 className="text-[#eee]">NEW FALL x COLLECTION</h1>
-          </div>
-          <div className="flex gap-5">
-            <div className="border p-1 rounded-2xl text-[14px] text-[#eee]">
-              NORTH FACE
-            </div>
-            <h1 className="text-[#eee]">NEW FALL x COLLECTION</h1>
-          </div>
-          <div className="flex gap-5">
-            <div className="border p-1 rounded-2xl text-[14px] text-[#eee]">
-              NORTH FACE
-            </div>
-            <h1 className="text-[#eee]">NEW FALL x COLLECTION</h1>
-          </div>
-          <div className="flex gap-5">
-            <div className="border p-1 rounded-2xl text-[14px] text-[#eee]">
-              NORTH FACE
-            </div>
-            <h1 className="text-[#eee]">NEW FALL x COLLECTION</h1>
-          </div>
-          <div className="flex gap-5">
-            <div className="border p-1 rounded-2xl text-[14px] text-[#eee]">
-              NORTH FACE
-            </div>
-            <h1 className="text-[#eee]">NEW FALL x COLLECTION</h1>
-          </div>
-          <div className="flex gap-5">
-            <div className="border p-1 rounded-2xl text-[14px] text-[#eee]">
-              NORTH FACE
-            </div>
-            <h1 className="text-[#eee]">NEW FALL x COLLECTION</h1>
           </div>
         </div>
       </div>
